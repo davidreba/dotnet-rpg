3b97415debc1cdf0c8159a5ffad878bcc6ca6557
David Reba <reba@hotmail.rs>
2024-01-24 19:06:50 +0100

    new file:   Controllers/CharacterController.cs 	new file:   Models/Character.cs 	new file:   Models/RpgClass.cs 	modified:   Program.cs 	new file:   Services/CharacterService/CharacterService.cs 	new file:   Services/CharacterService/ICharacterService.cs

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private readonly ICharacterService _characterService;
+        public CharacterController(ICharacterService caracterService)
+        {
+            _characterService = caracterService;            
+        }
+        
+        [HttpGet]
+        [Route("GetAll")]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSignle(int id)
+        {
+            return Ok(_characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            return Ok(_characterService.AddCharacter(newCharacter));
+        }
+    }
+}
